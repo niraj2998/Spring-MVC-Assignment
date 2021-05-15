66031d35af1b19b5be456177db1c2acc05df6d1f
niraj2998 <nirajpatil2998@gmail.com>
2021-05-15 11:02:09 +0530

    added files

---
+++ Hello-World-Spring/src/main/java/com/hello/controller/CrunchifyHelloWorld.java
@@ -0,0 +1,22 @@
+package com.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+/*
+ * author: Crunchify.com
+ * 
+ */
+ 
+@Controller
+public class CrunchifyHelloWorld {
+ 
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3> Hello World,</h3>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
